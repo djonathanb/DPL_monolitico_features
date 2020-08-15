5db2f7cbd350fa829bc483dc686b266c79754a67
Djonathan Barros <djonathan.barros@gmail.com>
2020-08-15 17:21:06 -0300

    identified feature code on Main class

--- features/Base/Main.java
+++ features/Base/Main.java
@@ -20,17 +20,26 @@ public class Main extends JFrame {
 	protected JPanel toolPanel = new JPanel();
 	protected Canvas canvas = new Canvas();
 	private Container contentPane;
+	// TODO Line
 	private JButton lineButton;
+	// TODO Rect
 	private JButton rectButton;
+	// TODO Wipe
 	private JButton wipeButton;
+	// TODO Wipe
 	private static final String wipeText = "   Limpar   ";
+	// TODO Rect
 	private static final String rectText = "Retângulo";
+	// TODO Line
 	private static final String lineText = "    Linha    ";
 
 	// *** Initialization of atomic elements
 	public void initAtoms() {
+		// TODO Line
 		lineButton = new JButton(lineText);
+		// TODO Rect
 		rectButton = new JButton(rectText);
+		// TODO Wipe
 		wipeButton = new JButton(wipeText);
 	}
 
@@ -45,8 +54,11 @@ public class Main extends JFrame {
 
 	/** Initializes the content pane */
 	public void initContentPane() {
+		// TODO Line
 		toolPanel.add(lineButton);
+		// TODO Rect
 		toolPanel.add(rectButton);
+		// TODO Wipe
 		toolPanel.add(wipeButton);
 		contentPane.add(toolPanel, BorderLayout.WEST);
 		contentPane.add(canvas, BorderLayout.CENTER);
@@ -54,18 +66,21 @@ public class Main extends JFrame {
 
 	public void initListeners() {
 
+		// TODO Line
 		lineButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				canvas.selectedFigure(Canvas.FigureTypes.LINE);
 			}
 		});
 
+		// TODO Rect
 		rectButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				canvas.selectedFigure(Canvas.FigureTypes.RECT);
 			}
 		});
 
+		// TODO Wipe
 		wipeButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				canvas.wipe();
@@ -82,7 +97,7 @@ public class Main extends JFrame {
 	}
 
 	/* Constructor. No need to modify */
-	// TODO: Identify features
+	// TODO Identify features
 	public Main(String appTitle) {
 		super(appTitle);
 		init();
